c8029b8b7d06daa266c1f14103ebecf27b96dc3f
mkofdwu <mkofdwu@gmail.com>
2020-07-29 20:47:09 +0800

    Initial commit

---
+++ src/main/java/com/example/javachessserver/JavaChessServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.javachessserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChessServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaChessServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javachessserver/JavaChessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javachessserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChessServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
